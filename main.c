fcbedfe8f4d26df8b5c59891591324c099377737
30916 <129143373+naheulli@users.noreply.github.com>
2023-03-28 05:08:51 +0000

    add or.c

--- main.c
+++ main.c
@@ -1,9 +1,9 @@
-#include <stdio.h>
+// #include <stdio.h>
 
-main() {
-  int a=15;
-  int b=20;
-  int c = a&b;
+// main() {
+//   int a=15;
+//   int b=20;
+//   int c = a&b;
 
-  printf("AND 연산 결과 : %d", c);
-}
+//   printf("AND 연산 결과 : %d", c);
+// }
