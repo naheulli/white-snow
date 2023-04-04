0ab74beeed412656235d4840cdc1e0c14009a810
30916 <>
2023-04-04 04:25:33 +0000

    Initial commit

--- or.c
+++ or.c
@@ -1,9 +1,23 @@
-#include <stdio.h>
+// #include <stdio.h>
+// main(){
+//   int a=5, b=3;
 
-main() {
-  int a=15;
-  int b=20;
-  int c = a|b;
+//   if(5 > 3)  
+//     printf("A\n");
+//   printf("B\n");
 
-  printf("AND 연산 결과 : %d", c);
-}
+//   if(5 + 3) 
+//     printf("C\n");
+//   printf("D\n");
+
+//   if(5 - 5) 
+//     printf("E\n");
+
+//    if(a > b) 
+//     printf("F\n");
+//   printf("G\n");
+
+//    if(a < 3) 
+//     printf("H\n");
+//   printf("I\n");  
+// }
