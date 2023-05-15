f8947780e9b43652ad8f4d944c8163ae142cf146
naheulli <129143373+naheulli@users.noreply.github.com>
2023-05-15 02:17:28 +0000

    switch문 추

---
+++ .ccls-cache/@home@runner@white-snow/mission1.c
@@ -0,0 +1,14 @@
+// #include <stdio.h>
+// main(){
+//   int a;
+  
+//   printf("하나의 정수를 입력하세요 :");
+//   scanf("%d", &a);
+
+//   if(a%2==0){
+//     printf("짝수입니다");
+//   }
+//   else{
+//     printf("홀수입니다");
+//   }
+// }
---
+++ else_if_score.c
@@ -0,0 +1,25 @@
+// #include <stdio.h>
+
+// main(){
+
+//   int a;
+//   printf("점수를 입력해주세요\n");
+//   scanf("%d", &a);
+  
+//   if(a>=90)
+//     printf("A학점\n");
+
+//   else if (a>=80)
+//     printf("B학점\n");
+
+//   else if (a>=70)
+//     printf("C학점\n");
+
+//   else if (a>=60)
+//     printf("D학점\n");
+
+//   else
+//     printf("F학점\n");
+
+//   printf("재밌는 성적 조사 끝!!");
+// }
